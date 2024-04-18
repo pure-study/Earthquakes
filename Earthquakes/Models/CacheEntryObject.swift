cc2f9d55833990bee3537d644738896f8b170463
Will <yzj_000@163.com>
2024-04-18 21:51:37 +0800

    Initial commit

---
+++ Earthquakes/Models/CacheEntryObject.swift
@@ -0,0 +1,18 @@
+//
+//  CacheEntryObject.swift
+//  Earthquakes
+//
+
+import Foundation
+
+final class CacheEntryObject {
+    let entry: CacheEntry
+    init(entry: CacheEntry) {
+        self.entry = entry
+    }
+}
+
+enum CacheEntry {
+    case inProgress(Task<QuakeLocation, Error>)
+    case ready(QuakeLocation)
+}
---
+++ Earthquakes/Preview Content/TestDownloader.swift
@@ -0,0 +1,13 @@
+//
+//  TestDownloader.swift
+//  Earthquakes
+//
+
+import Foundation
+
+class TestDownloader: HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+        try await Task.sleep(nanoseconds: UInt64.random(in: 100_000_000...500_000_000))
+        return testQuakesData
+    }
+}
